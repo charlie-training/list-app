955b1b007b2b8715d83b7fddcbe9d38e4c9e9176
charlie-training <charlie-training@protonmail.com>
2025-09-21 08:37:02 +0100

    app fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { createListItem } from './listDAO'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [listItems, setListItems] = useState([])
+  const [listItems,] = useState([])
 
   return (
     <>
