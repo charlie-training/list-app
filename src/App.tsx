a728fd631d084e9c8d1f83d01a68a7cd05cf77b9
Charlie <charlie-training@proton.me>
2025-09-27 21:42:17 +0100

    refresh booton

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { getAllListItems } from './listDAO'
 import { ApiGuard } from './ApiGuard'
 import Login from './Login'
 import ListItemRow, { AddRow } from './ListItem'
+import refresh from "./assets/refresh.svg"
 
 function App() {
   const [listItems, setListItems] = useState([])
@@ -22,9 +23,26 @@ function App() {
   if (!apiKey) return <Login />
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", padding: "1rem", maxWidth: "90vw", justifyItems: "center" }}>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      padding: "1rem",
+      maxWidth: "90vw",
+      justifyItems: "center"
+    }}>
+
       <ApiGuard>
-        <h1 style={{ textAlign: "center" }}>To-do List</h1>
+        <h1 style={{ textAlign: "center", marginBottom: "1rem" }}>To-do List</h1>
+        <div style={{ display: "flex", cursor: "pointer" }} onClick={() => setRefreshKey(refreshKey + 1)}>
+          <img style={{
+            width: "2rem",
+            marginRight: "0",
+            marginLeft: "auto",
+            borderStyle: "solid",
+            padding: "0.2rem",
+            borderRadius: "0.5rem"
+          }} src={refresh} />
+        </div>
         <div className='allNotes'>
           {listItems.map((item, index) => <ListItemRow
             key={index}
